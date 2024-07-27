8be19c767b890f9b714412fb017e46e1d5e92c65
Freelancer0708 <freelancernoaccont@gmail.com>
2024-07-27 23:06:06 +0900

    fix css

--- pages/user/plans/[id].tsx
+++ pages/user/plans/[id].tsx
@@ -48,18 +48,22 @@ const PlanDetail = () => {
     if (!plan) return <div>Loading...</div>;
 
     return (
-        <div>
-            {plan.imageUrl && <img src={plan.imageUrl} alt={plan.planName} style={{ width: 'auto', height: 'auto', maxHeight: '200px' }} />}
-            <h1>{plan.planName}</h1>
-            <p>ホテル名: {plan.hotelName}</p>
-            <p>宿泊期間: {formatStayPeriod(plan.startDate, plan.days)}</p>
-            <p>値段: {plan.price}</p>
-            <p>残りの部屋数: {availableRooms !== null ? availableRooms : 'Loading...'}</p>
-            {availableRooms !== null && availableRooms > 0 ? (
-                <a href={`/user/plans/${id}/reserve`}>予約する</a>
-            ) : (
-                <p>予約できる部屋がありません。</p>
-            )}
+        <div className='user-detail'>
+            <h2>{plan.planName}</h2>
+            <div className='detail-row'>
+                {plan.imageUrl && <img src={plan.imageUrl} alt={plan.planName} style={{ width: 'auto', height: 'auto', maxHeight: '200px' }} />}
+                <div className='detail-item'>
+                    <p>ホテル名: {plan.hotelName}</p>
+                    <p>宿泊期間: {formatStayPeriod(plan.startDate, plan.days)}</p>
+                    <p>値段: {plan.price}</p>
+                    <p>残りの部屋数: {availableRooms !== null ? availableRooms : 'Loading...'}</p>
+                    {availableRooms !== null && availableRooms > 0 ? (
+                        <a href={`/user/plans/${id}/reserve`}>予約する</a>
+                    ) : (
+                        <p>予約できる部屋がありません。</p>
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
--- pages/user/plans/[id]/complete.tsx
+++ pages/user/plans/[id]/complete.tsx
@@ -2,9 +2,14 @@ import withAuthUser from "../../../../hoc/withAuthUser";
 
 const ReservationComplete = () => {
     return (
-        <div>
-            <h1>予約登録完了</h1>
-            <p>予約が完了しました。ありがとうございました。</p>
+        <div className="user-complete">
+            <h2>予約登録完了</h2>
+            <p>
+                予約が完了いたしました。<br />
+                10分以内に登録したメールアドレスに<br/>
+                予約完了メールをが届きますので、<br/>
+                ご確認いただけますと幸いです。
+            </p>
         </div>
     );
 };
